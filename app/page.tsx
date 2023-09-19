94849035f790ed3d3eeacb30b10adaddddeeb67e
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-09-19 23:17:13 +0530

    Portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import Image from 'next/image'
+import Header from '@/components/Header'
 
 export default function Home() {
   return (
-    
-      <h1 className='p-10 text-red-500'>Lets Build AWESOME portfolio </h1> 
-    
+    <div className="Home">
+
+      {/*Header*/}
+      <Header/>
+      {/*Hero*/}
+      {/*About*/}
+      {/*Experience*/}
+      {/*Skills*/}
+      {/*Projects*/}
+      {/*Contact Me*/}
+    </div>
+
   )
 }
---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function Header({}: Props) {
+  return (
+    <div>I m ur Header</div>
+  )
+}
+
+export default Header
